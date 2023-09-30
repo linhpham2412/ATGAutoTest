d07c10e7e19a589a60eaa1eb3913886c4e9e5943
linhpham2412 <blue.linhpham2412@gmail.com>
2023-10-01 00:03:18 +0700

    Create repo and start with test case 1

---
+++ untitled/src/main/java/atg/automation/selenium/Constant.java
@@ -0,0 +1,10 @@
+package atg.automation.selenium;
+
+import java.time.Duration;
+public class Constant {
+    public static final long SHORT_TIME = 3;
+    public static final Duration SHORT_DURATION = Duration.ofSeconds(SHORT_TIME);
+    public static final long MEDIUM_TIME = 10;
+    public static final long LONG_TIME = 20;
+    public static final long SUPER_LONG_TIME = 40;
+}
